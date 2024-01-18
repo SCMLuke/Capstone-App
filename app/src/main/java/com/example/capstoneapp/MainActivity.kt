c5162e9aec1a4fa5ad45e64357f85fc8b8637e77
Mark Deyarmond <deyarmondmark@gmail.com>
2024-01-18 12:27:57 -0400

    Test commit

--- app/src/main/java/com/example/capstoneapp/MainActivity.kt
+++ app/src/main/java/com/example/capstoneapp/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
+        // Testing for a commit
         text = "Hello $name!",
         modifier = modifier
     )
