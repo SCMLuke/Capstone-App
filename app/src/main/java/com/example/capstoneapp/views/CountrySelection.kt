2b77f6f40749545e44653f074fee0e7ce5f9010b
Mark Deyarmond <deyarmondmark@gmail.com>
2024-02-13 13:30:08 -0400

    Inserted sticky header, each country card now uses Expandable cards

--- app/src/main/java/com/example/capstoneapp/views/CountrySelection.kt
+++ app/src/main/java/com/example/capstoneapp/views/CountrySelection.kt
@@ -1,24 +1,27 @@
 package com.example.capstoneapp.views
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -26,72 +29,93 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class CountrySelection {
+    @OptIn(ExperimentalFoundationApi::class)
     @Composable
     fun LandingScreen(modifier: Modifier = Modifier) {
-        val countries = listOf("America", "Argentina", "Bhutan", "Canada", "France", "Greece", "Iran", "Mexico", "Netherlands", "Sweden")
+        val countries = listOf(
+            "America",
+            "Argentina",
+            "Australia",
+            "Bhutan",
+            "Brazil",
+            "Canada",
+            "China",
+            "France",
+            "Germany",
+            "Greece",
+            "India",
+            "Iran",
+            "Italy",
+            "Japan",
+            "Mexico",
+            "Netherlands",
+            "Russia",
+            "Sweden"
+        )
 
         LazyColumn(
-            modifier = modifier
-                .fillMaxSize()
-                .background(color = Color(0xff273537))
+            modifier = Modifier.background(Color(0xff727272))
         ) {
-            item {
+            stickyHeader {
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .requiredHeight(height = 90.dp)
                         .background(color = Color(0xff254954))
                 ) {
                     Text(
                         text = "Country Selection",
                         color = Color.White,
-                        textAlign = TextAlign.Center,
+                        textAlign = TextAlign.Start,
                         style = TextStyle(
-                            fontSize = 48.sp,
+                            fontSize = 32.sp,
                             fontWeight = FontWeight.Bold,
-                            shadow = Shadow(
-                                color = Color.Black.copy(alpha = 0.25f),
-                                offset = Offset(0f, 4f),
-                                blurRadius = 4f
-                            )
                         ),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .requiredHeight(height = 90.dp)
-                            .border(border = BorderStroke(1.dp, Color(0xff282828)))
-                            .wrapContentHeight(align = Alignment.CenterVertically)
+                        modifier = Modifier.padding(16.dp)
                     )
                 }
             }
 
             items(countries.size) { index ->
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .requiredHeight(85.dp)
-                        .padding(vertical = 8.dp, horizontal = 20.dp)
-                        .clip(shape = RoundedCornerShape(25.dp))
-                        .background(color = Color(0xff8db2b2))
-                ) {
-                    Text(
-                        text = countries[index],
-                        color = Color(0xff0f0f0f),
-                        textAlign = TextAlign.Center,
-                        style = TextStyle(
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Light
-                        ),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(align = Alignment.CenterVertically)
-                    )
-                }
+                ExpandableCard(country = countries[index])
             }
         }
     }
 
+    // Compose code for the expandable card function
     @Composable
-    private fun LandingScreenPreview() {
-        CountrySelection().LandingScreen()
+    fun ExpandableCard(country: String) {
+        var expanded by remember { mutableStateOf(false) }
+
+        ElevatedCard(
+            shape = RoundedCornerShape(8.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color(0xFF89E9F3)
+            ),
+            elevation = CardDefaults.cardElevation(
+               defaultElevation = 8.dp
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 3.dp, horizontal = 8.dp)
+                .clickable { expanded = !expanded }
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = country,
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Light
+                    )
+                )
+
+                // Code block to handle expansion event of card
+                if (expanded) {
+                    // TODO: Add some code
+                }
+            }
+        }
     }
 }
