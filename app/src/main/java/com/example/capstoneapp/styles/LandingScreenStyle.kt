31f8ecb75d110f962058c7a2e86c1afc31780bc7
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-05 12:06:47 -0400

    Created some global UI styles to apply

--- app/src/main/java/com/example/capstoneapp/styles/LandingScreenStyle.kt
+++ app/src/main/java/com/example/capstoneapp/styles/LandingScreenStyle.kt
@@ -1,14 +1,19 @@
 package com.example.capstoneapp.styles
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 class LandingScreenStyle {
 
     object BodyStyle {
-        val backgroundColor: Color = Color(0xFF202020)
+        val backgroundColor: Color = Color(0xFF474747)
     }
     object HeaderStyle {
         val backgroundColor: Color = Color(0xFF6E7E85)
         val textColor: Color = Color(0xFFD1D1D1)
+    }
+    object ExpandedCardStyle {
+        val textColor: Color = Color(0xFFD1D1D1)
+        val fontSize = 16.sp
     }
 }
