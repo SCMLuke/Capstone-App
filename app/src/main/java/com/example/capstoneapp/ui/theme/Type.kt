060c10b60b35fb99d0ae020fa0ebc08466146542
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-12 16:29:53 -0300

    Implemented fonts for global use

--- app/src/main/java/com/example/capstoneapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/capstoneapp/ui/theme/Type.kt
@@ -2,9 +2,32 @@ package com.example.capstoneapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.capstoneapp.R
+
+// Font families
+val Noto = FontFamily(
+    Font(R.font.notosansdisplay_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.notosansdisplay_italic, FontWeight.Normal, FontStyle.Italic)
+)
+
+val RedhatDisplay = FontFamily(
+    Font(R.font.redhatdisplay_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.redhatdisplay_italic, FontWeight.Normal, FontStyle.Italic)
+)
+
+val Nunito = FontFamily(
+    Font(R.font.nunito_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.nunito_italic, FontWeight.Normal, FontStyle.Italic)
+)
+
+val Syne = FontFamily(
+    Font(R.font.syne_regular, FontWeight.Normal, FontStyle.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
