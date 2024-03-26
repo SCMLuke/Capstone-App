84bc4d74657e1e6f38d2006cab3e15c5e93559c3
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-26 15:03:33 -0300

    Back to basics, minimal functionality working

--- app/src/main/java/com/example/capstoneapp/views/MapScreen.kt
+++ app/src/main/java/com/example/capstoneapp/views/MapScreen.kt
@@ -1,3 +1,5 @@
+package com.example.capstoneapp.views
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -5,50 +7,44 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.ktx.model.cameraPosition
 
-@Composable
-fun MapScreen(
-    locationName: String,
-    locationDescription: String,
-    latitude: Double,
-    longitude: Double,
-    navController: NavController
-) {
-    val cameraPositionState = rememberCameraPositionState {
-        cameraPosition {
-            target(LatLng(latitude, longitude))
-            zoom(10f)
+class MapScreen {
+    companion object {
+        @Composable
+        fun Content(navController: NavController) {
+            val cameraPositionState = rememberCameraPositionState {
+                cameraPosition {
+                    target(LatLng(DEFAULT_LATITUDE, DEFAULT_LONGITUDE))
+                    zoom(10f)
+                }
+            }
+
+            Column(modifier = Modifier.fillMaxSize()) {
+                GoogleMap(
+                    modifier = Modifier.fillMaxSize(),
+                    cameraPositionState = cameraPositionState
+                )
+
+                Button(
+                    onClick = { navController.navigateUp() },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Text(text = "Return")
+                }
+            }
         }
-    }
-    
-    Column(modifier = Modifier.fillMaxSize()) {
-        GoogleMap (
-            modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState
-        ) {
-            Marker(
-                state = MarkerState(position = LatLng(latitude, longitude)),
-                title = locationName,
-                snippet = locationDescription
-            )
-        }
-        
-        Button(
-            onClick = { navController.navigateUp() },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Text(text = "Return")
-        }
+
+        // Dummy co-ordinates for now
+        private const val DEFAULT_LATITUDE = 0.0
+        private const val DEFAULT_LONGITUDE = 0.0
     }
 }
